bbf69f314c3c44426702f253d6a00f8cc0d291a2
aammddkkzxc <ideal0427@naver.com>
2024-08-07 00:17:39 +0900

    feat: 다양한 의존관계 주입 방법, 테스트 수정 생성자가 딱 1개만 있으면 @Autowired를 생략해도 자동 주입 된다 (스프링 빈에만 해당) 필드 주입의 경우 테스트시 변경이 아예 불가능 해서 테스트가 힘들다

--- core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
@@ -16,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextExtendsFindTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(Config.class);
 
     @Test
     @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복 오류가 발생한다")
@@ -64,10 +65,12 @@ public class ApplicationContextExtendsFindTest {
 
     @Configuration
     static class Config {
+        @Bean
         public DiscountPolicy rateDiscountPolicy() {
             return new RateDiscountPolicy();
         }
 
+        @Bean
         public DiscountPolicy fixDiscountPolicy() {
             return new FixDiscountPolicy();
         }
