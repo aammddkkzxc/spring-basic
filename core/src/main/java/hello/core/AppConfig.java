378169618048b2269f39e04f8a6953818681d8c3
aammddkkzxc <ideal0427@naver.com>
2024-08-07 21:00:12 +0900

    feat: request 스코프 예제 만들기 - request scope를 사용하지 않고 파라미터로 이 모든 정보를 서비스 계층에 넘긴다면, 파라미터가 많아서 지저분해진다. - 더 문제는 requestURL 같은 웹과 관련된 정보가 웹과 관련없는 서비스 계층까지 넘어가게 된다. 웹과 관련된 부분은 컨트롤러까지만 사용해야 한다. 서비스 계층은 웹 기술에 종속되지 않고, 가급적 순수하게 유지하는 것이 유지보수 관점에서 좋다
    - 스프링 애플리케이션을 실행하는 시점에 싱글톤 빈은 생성해서 주입이 가능하지만, request 스코프 빈은 아직 생성되지 않는다. 이 빈은 실제 고객의 요청이 와야 생성됨.

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -14,27 +14,27 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public MemberService memberService() {
-        System.out.println("call memberService");
-        return new MemberServiceImpl(memberRepository());
-    }
-
-    @Bean
-    public OrderService orderService() {
-        System.out.println("call orderService");
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        System.out.println("call memberRepository");
-        return new MemoryMemberRepository();
-    }
-
-    @Bean
-    public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
-        return new RateDiscountPolicy();
-    }
+//    @Bean
+//    public MemberService memberService() {
+//        System.out.println("call memberService");
+//        return new MemberServiceImpl(memberRepository());
+//    }
+//
+//    @Bean
+//    public OrderService orderService() {
+//        System.out.println("call orderService");
+//        return new OrderServiceImpl(memberRepository(), discountPolicy());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        System.out.println("call memberRepository");
+//        return new MemoryMemberRepository();
+//    }
+//
+//    @Bean
+//    public DiscountPolicy discountPolicy() {
+////        return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
+//    }
 }
